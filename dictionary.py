55b64f0e4d54f1fd5108d3c667756ededccbc280
wpoon426 <wpoon426@mtroyal.ca>
2024-09-25 12:43:36 -0600

    initializing the DB in python.
    included the transaction queue as well in the file.

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+
+#Initializing the database
+database = [
+    {'Unique_ID': 1, 'First_name': 'John', 'Last_name': 'Lennon', 'Salary': 230000, 'Department': 'Projects', 'Civil_status': 'Married'},
+    {'Unique_ID': 2, 'First_name': 'Joan', 'Last_name': 'Doe', 'Salary': 100000, 'Department': 'Human Resources', 'Civil_status': 'Single'},
+    {'Unique_ID': 3, 'First_name': 'Mary', 'Last_name': 'Carpenter', 'Salary': 250000, 'Department': 'Projects', 'Civil_status': 'Separated'},
+    {'Unique_ID': 4, 'First_name': 'John', 'Last_name': 'Ingham', 'Salary': 125000, 'Department': 'Projects', 'Civil_status': 'Separated'},
+    {'Unique_ID': 5, 'First_name': 'Rachel', 'Last_name': 'Sturgeon', 'Salary': 197000, 'Department': 'Engineering', 'Civil_status': 'Married'},
+    {'Unique_ID': 6, 'First_name': 'Hanifa', 'Last_name': 'Salima', 'Salary': 50000, 'Department': 'Engineering', 'Civil_status': 'Married'},
+    {'Unique_ID': 7, 'First_name': 'Femi', 'Last_name': 'Okeke', 'Salary': 425000, 'Department': 'Industries', 'Civil_status': 'Married'},
+    {'Unique_ID': 8, 'First_name': 'Moe', 'Last_name': 'Khalifa', 'Salary': 325000, 'Department': 'Industries', 'Civil_status': 'Married'},
+    {'Unique_ID': 9, 'First_name': 'Katy', 'Last_name': 'Jones', 'Salary': 475000, 'Department': 'Management', 'Civil_status': 'Single'},
+    {'Unique_ID': 10, 'First_name': 'Lin', 'Last_name': 'Wang', 'Salary': 435000, 'Department': 'Engineering', 'Civil_status': 'Married'},
+    {'Unique_ID': 11, 'First_name': 'Art', 'Last_name': 'Blanket', 'Salary': 137000, 'Department': 'Projects', 'Civil_status': 'Single'},
+    {'Unique_ID': 12, 'First_name': 'Vivek', 'Last_name': 'Singh', 'Salary': 231000, 'Department': 'Industries', 'Civil_status': 'Married'},
+    {'Unique_ID': 13, 'First_name': 'Amal', 'Last_name': 'Khan', 'Salary': 230000, 'Department': 'Projects', 'Civil_status': 'Single'},
+    {'Unique_ID': 14, 'First_name': 'Richard', 'Last_name': 'Carpenter', 'Salary': 123000, 'Department': 'Human Resources', 'Civil_status': 'Single'},
+    {'Unique_ID': 15, 'First_name': 'Ryuichi', 'Last_name': 'Sakamoto', 'Salary': 321000, 'Department': 'Processing Facilities', 'Civil_status': 'Single'}
+]
+
+#transaction que
+transaction = [
+    {'ID': 1, 'Attribute': 'Department', 'New_value': 'Music'},
+    {'ID': 5, 'Attribute': 'Civil_status', 'New_value': 'Divorced'},
+    {'ID': 15, 'Attribute': 'Salary', 'New_value': 200000}
+]
